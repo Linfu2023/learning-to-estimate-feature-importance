ed353d8438f026779cf839afad940086a7b71df3
frank.qc <frank.qc@antgroup.com>
2023-10-15 11:12:11 +0800

    bug fixing

--- train/train_clf_LTE.sh
+++ train/train_clf_LTE.sh
@@ -1,8 +1,1 @@
 python train_LambdaRank_LTE.py --directory ../ --output_dir models/temp_clf_LTE_models --type classification
-
-        modified:   predict/predict.py
-        modified:   predict/run_predict.sh
-        modified:   test/eval_on_test_data.py
-        modified:   test/eval_util.py
-        modified:   train/train_LambdaRank_LTE.py
-        modified:   train/train_clf_LTE.sh
